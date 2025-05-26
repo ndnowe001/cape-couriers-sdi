f29d685829e7d8ea25836eb563567c69a53247f2
Owethu Ndongeni <ndnowe001@myuct.ac.za>
2025-05-26 17:00:43 +0200

    store finder upload

---
+++ storefinder/data/stores_1.js
@@ -0,0 +1,15 @@
+var json_stores_1 = {
+"type": "FeatureCollection",
+"name": "stores_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "store_id": 1.0, "name": "Observatory Store", "address": "Shop 19 St Peters Square, Main Rd, Observatory, Cape Town, 7925", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.94187057, "Longitude": 18.4665511 }, "geometry": { "type": "Point", "coordinates": [ 18.4665511, -33.94187057 ] } },
+{ "type": "Feature", "properties": { "store_id": 2.0, "name": "Tabe View Store", "address": "Cnr Blaauwberg Rd & Study St Shop 7, Flamingo Square, Table View, Cape Town, 7439", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.83053816, "Longitude": 18.50626871 }, "geometry": { "type": "Point", "coordinates": [ 18.50626871, -33.83053816 ] } },
+{ "type": "Feature", "properties": { "store_id": 3.0, "name": "Parow East Store", "address": "Cnr Voortrekker & De La Ray Rd Shop 141, Parow Centre, Parow East, Cape Town, 7501", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.90511328, "Longitude": 18.59926958 }, "geometry": { "type": "Point", "coordinates": [ 18.59926958, -33.90511328 ] } },
+{ "type": "Feature", "properties": { "store_id": 4.0, "name": "City Centre Store", "address": "Shop 8, 2 Long St, Cape Town City Centre, Cape Town, 8001", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.91862252, "Longitude": 18.42315598 }, "geometry": { "type": "Point", "coordinates": [ 18.42315598, -33.91862252 ] } },
+{ "type": "Feature", "properties": { "store_id": 5.0, "name": "Kenilworth Store", "address": "Shop 80, 1 Doncaster Rd, Kenilworth, Cape Town, 7708", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.98956792, "Longitude": 18.48449601 }, "geometry": { "type": "Point", "coordinates": [ 18.48449601, -33.98956792 ] } },
+{ "type": "Feature", "properties": { "store_id": 6.0, "name": "Milnerton Store", "address": "Mall, Cnr Koeberg &, Loxton Rd, Milnerton, Cape Town, 7441", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.8829025, "Longitude": 18.49546266 }, "geometry": { "type": "Point", "coordinates": [ 18.49546266, -33.8829025 ] } },
+{ "type": "Feature", "properties": { "store_id": 7.0, "name": "Kuilsriver Store", "address": "Shop 7, Kerkplein Centre, 53 Van Riebeeck Rd, Kuils River, Cape Town, 7580", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.92466135, "Longitude": 18.67865917 }, "geometry": { "type": "Point", "coordinates": [ 18.67865917, -33.92466135 ] } },
+{ "type": "Feature", "properties": { "store_id": 8.0, "name": "Rylands Store", "address": "Shop 9, 51, Rylands Shopping Centre, 59 College Rd, Rylands, Cape Town, 7764", "hours": "Mon-Fri: 08:00 - 17:00 , Sat: 09:00-13:00", "Latitude": -33.97459776, "Longitude": 18.53008206 }, "geometry": { "type": "Point", "coordinates": [ 18.53008206, -33.97459776 ] } }
+]
+}
