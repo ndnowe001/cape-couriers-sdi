92eec1dcc9d473f02113e95c06c8fa1a0cf1e51e
Owethu Ndongeni <ndnowe001@myuct.ac.za>
2025-05-29 16:31:26 +0200

    Route map upload

---
+++ routemap/data/warehouses_10.js
@@ -0,0 +1,8 @@
+var json_warehouses_10 = {
+"type": "FeatureCollection",
+"name": "warehouses_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "warehouse_code": 100.0, "name": "Main Warehouse", "address": "4 Topaz Blvd, Milnerton, Cape Town, 7441", "latitude": -33.85071, "longitude": 18.52668 }, "geometry": { "type": "Point", "coordinates": [ 18.52668, -33.85071 ] } }
+]
+}
